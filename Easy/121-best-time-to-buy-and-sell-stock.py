03504a28b622873b013b11d571acadd330128908
rameshdragon <Rameshdragonreddy@gmail.com>
2026-05-01 16:15:58 +0000

    Add 121 Best Time to Buy and Sell Stock - Easy, 002 Add Two Numbers - Medium

---
+++ Easy/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,15 @@
+# 121. Best Time to Buy and Sell Stock
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# Difficulty: Easy
+
+def maxProfit(prices: list[int]) -> int:
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+
+    return max_profit
