2cfd45421c0b6316e7540295ea1f378692648fe7
rameshdragon <Rameshdragonreddy@gmail.com>
2026-05-13 19:07:22 +0000

    Add 338 Counting Bits - Easy, 055 Jump Game - Medium

---
+++ Easy/338-counting-bits.py
@@ -0,0 +1,9 @@
+# 338. Counting Bits
+# https://leetcode.com/problems/counting-bits/
+# Difficulty: Easy
+
+def countBits(n: int) -> list[int]:
+    dp = [0] * (n + 1)
+    for i in range(1, n + 1):
+        dp[i] = dp[i >> 1] + (i & 1)
+    return dp
---
+++ Medium/055-jump-game.py
@@ -0,0 +1,11 @@
+# 55. Jump Game
+# https://leetcode.com/problems/jump-game/
+# Difficulty: Medium
+
+def canJump(nums: list[int]) -> bool:
+    max_reach = 0
+    for i, n in enumerate(nums):
+        if i > max_reach:
+            return False
+        max_reach = max(max_reach, i + n)
+    return True
